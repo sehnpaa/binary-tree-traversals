309663afb28ccab714e4de6f528bb0841c72f79e
Peter Andersson <sehnpaa@gmail.com>
2018-08-10 15:19:29 +0200

    Fix buggy breath first impl

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import Data.List (sortBy)
+
 data Tree a = Leaf a | Branch (Tree a) a (Tree a)
 
 t :: Num a => Tree a
@@ -22,14 +24,15 @@ inorder (Branch left a right) = inorder left ++ a : inorder right
 
 -- breathFirst t: [1,2,3,4,5]
 breathFirst :: Tree a -> [a]
-breathFirst (Leaf a) = [a]
-breathFirst (Branch (Leaf l) a (Leaf r)) = a : l : r : []
-breathFirst (Branch (Branch left l right) a (Leaf r)) =
-    a : l : r : breathFirst left ++ breathFirst right
-breathFirst (Branch (Leaf l) a (Branch left r right)) =
-    a : l : r : breathFirst left ++ breathFirst right
-breathFirst (Branch (Branch left l right) a (Branch left' r right')) =
-    a : l : r : breathFirst left ++ breathFirst right ++ breathFirst left' ++ breathFirst right'
+breathFirst = map fst . sortBySnd . tagTree 0
+
+tagTree :: Int -> Tree a -> [(a, Int)]
+tagTree n (Leaf a) = [(a, n)]
+tagTree n (Branch left a right) =
+    (a,n) : tagTree (n+1) left ++ tagTree (n+1) right
+
+sortBySnd :: Ord b => [(a, b)] -> [(a, b)]
+sortBySnd = sortBy (\a b -> compare (snd a) (snd b))
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
